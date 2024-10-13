2c113afe126d26d264cdbd9bbac8153b74c02511
Taric Somani <taric.somani@gmail.com>
2024-10-13 18:04:18 -0400

    initial commit

---
+++ rock-paper-scissors.js
@@ -0,0 +1,35 @@
+function rock_paper_scissors(userChoice) {
+    let choices = ['rock', 'paper', 'scissors'];
+    let randomIndex = Math.floor(Math.random() * 3);
+    let computerChoice = choices[randomIndex];
+    
+    // Update the computer choice image
+    document.getElementById('computer-choice-img').src = `${computerChoice}.png`;
+    document.getElementById('computer-choice-img').alt = computerChoice;
+
+    if (userChoice === computerChoice) {
+        document.getElementById('result').innerText = 'It is a tie';
+    } else if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            document.getElementById('result').innerText = 'Computer wins';
+        } else {
+            document.getElementById('result').innerText = 'You win';
+        }
+    } else if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            document.getElementById('result').innerText = 'Computer wins';
+        } else {
+            document.getElementById('result').innerText = 'You win';
+        }
+    } else if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            document.getElementById('result').innerText = 'Computer wins';
+        } else {
+            document.getElementById('result').innerText = 'You win';
+        }
+    }
+}
+
+function play(userChoice) {
+    rock_paper_scissors(userChoice);
+}
