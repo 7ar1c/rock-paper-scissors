fe2c3d6d61195358646764b0c992466e30ea42fc
Taric Somani <taric.somani@gmail.com>
2024-10-13 18:29:54 -0400

    added win tally

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,6 @@
+let wins = 0;
+let losses = 0;
+let ties = 0;
 function rock_paper_scissors(userChoice) {
     let choices = ['rock', 'paper', 'scissors'];
     let randomIndex = Math.floor(Math.random() * 3);
@@ -9,23 +12,37 @@ function rock_paper_scissors(userChoice) {
 
     if (userChoice === computerChoice) {
         document.getElementById('result').innerText = 'It is a tie';
+        ties++;
+        document.getElementById('ties').innerText = ties;
     } else if (userChoice === 'rock') {
         if (computerChoice === 'paper') {
             document.getElementById('result').innerText = 'Computer wins';
+            losses++;
+            document.getElementById('losses').innerText = losses;
         } else {
             document.getElementById('result').innerText = 'You win';
+            wins++;
+            document.getElementById('wins').innerText = wins;
         }
     } else if (userChoice === 'paper') {
         if (computerChoice === 'scissors') {
             document.getElementById('result').innerText = 'Computer wins';
+            losses++;
+            document.getElementById('losses').innerText = losses;
         } else {
             document.getElementById('result').innerText = 'You win';
+            wins++;
+            document.getElementById('wins').innerText = wins;
         }
     } else if (userChoice === 'scissors') {
         if (computerChoice === 'rock') {
             document.getElementById('result').innerText = 'Computer wins';
+            losses++;
+            document.getElementById('losses').innerText = losses;
         } else {
             document.getElementById('result').innerText = 'You win';
+            wins++;
+            document.getElementById('wins').innerText = wins;
         }
     }
 }
@@ -33,3 +50,4 @@ function rock_paper_scissors(userChoice) {
 function play(userChoice) {
     rock_paper_scissors(userChoice);
 }
+
